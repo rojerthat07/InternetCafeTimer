02f0b48319e2711f1296bedd088562e7e3432acb
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-04 16:30:37 +0800

    Update timer-section.component.ts

--- src/app/timer-section/timer-section.component.ts
+++ src/app/timer-section/timer-section.component.ts
@@ -5,16 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './timer-section.component.html',
   styleUrls: ['./timer-section.component.scss']
 })
-export class TimerSectionComponent {
+export class TimerSectionComponent implements OnInit {
 
-  constructor() { }
+
 
   //Date and time
   public date:any = new Date();
   public hours12Format:string = (this.date.getHours() > 12 ?  this.date.getHours() - 12 : this.date.getHours() );
 
 
-  
+  ngOnInit():void{
+ 
+  }
 
   //to diplay minutes with leading zeroes
 seconds_with_leading_zeros =(date = this.date) =>{ 
@@ -24,19 +26,21 @@ seconds_with_leading_zeros =(date = this.date) =>{
   //Forms Input 2 way data binding
   public formId:number = 1;
   public formName:string;
-  public formStart:string =  `${this.hours12Format}:${this.seconds_with_leading_zeros()}`;
+  public formStart:string =  `${this.hours12Format}:${this.seconds_with_leading_zeros()}:${this.date.getSeconds()}`;
   public formEnd:string;
   public formDuration:string;
   public formBalance:string;
 
   public inputs:any[] = []
 
-
+  constructor() { 
+   
+  }
 
   onFormSubmit = () =>{
     this.inputs.push({id:this.formId,name:this.formName,start:this.formStart,end:this.formEnd,duration:this.formDuration,balance:this.formBalance});
     this.formId++;
-  
+
   }
 
 }
