bd81aa2a714f49c81075a5b2bbf8d7a0e828ebf4
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-07-10 13:13:45 +0800

    cleaning the code in timer-section

--- src/app/timer-section/timer-section.component.ts
+++ src/app/timer-section/timer-section.component.ts
@@ -8,18 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerSectionComponent implements OnInit {
 
+  //to diplay minutes and seconds with leading zeroes
+  seconds_with_leading_zeros =(date = this.date) =>{ 
+    return (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+  }
+  minutes_with_leading_zeros =(date = this.date) =>{ 
+    return (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+  }
+
   //Date and time
   public date:any = new Date();
   public hours12Format:string = (this.date.getHours() > 12 ?  this.date.getHours() - 12 : this.date.getHours() );
 
-  //to diplay minutes with leading zeroes
-seconds_with_leading_zeros =(date = this.date) =>{ 
-  return (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
-}
+   //Forms Input 2 way data binding
+   public formId:number = 1;
+   public formName:string;
+   public formStart:string =  `${this.hours12Format}:${this.seconds_with_leading_zeros()}:${this.date.getSeconds()}`;
+   public formEnd:any;
+   public formDuration:number = null;
+   public formBalance:number;
+   public totalBalance:number = null;
+   public inputs:any[] = [];
+   public endTime:any[] = [];
 
-minutes_with_leading_zeros =(date = this.date) =>{ 
-  return (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-}
 
 ngOnInit():void{
   setInterval(()=>{
@@ -60,26 +71,7 @@ ngOnInit():void{
   },1000)
 }
 
-  //Sound effect for paying
 
-
-
- 
-
-  //Forms Input 2 way data binding
-  public formId:number = 1;
-  public formName:string;
-  public formStart:string =  `${this.hours12Format}:${this.seconds_with_leading_zeros()}:${this.date.getSeconds()}`;
-  public formEnd:any;
-  public formDuration:number = null;
-  public formBalance:number;
-  public totalBalance:number = null;
-  public inputs:any[] = [];
-  public endTime:any[] = [];
-
-  constructor() { 
-  
-  }
 
   onFormSubmit = () =>{
     //Pushing data to the main inputs Array
